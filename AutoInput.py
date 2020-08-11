c64e80e4eb28bef0539c66571a62f1b9324999fa
Gouki-0209 <55020456+Gouki-0209@users.noreply.github.com>
2020-08-11 09:29:53 +0900

    修正

--- AutoInput.py
+++ AutoInput.py
@@ -5,25 +5,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class AutoInput:
+class GoogleFormInput:
     def start_chrome():
         driver = webdriver.Chrome(
             executable_path=r'C:\Users\gouki\OneDrive\ドキュメント\chromedriver_win32\chromedriver.exe')
-        driver.maximize_window()
 
         driver.get(
             'https://docs.google.com/forms/d/e/1FAIpQLSf0Ovrhjdn4uXKL7zVSavVbEcbixPnDXxPlea3nWs23NRsRGA/viewform')
         return driver
 
     def login_google(driver):
-        login_id = "m18013@g.metro-cit.ac.jp"
-        login_pw = "GoukiRuby0209"
+        login_id = "適当"
+        login_pw = "適当"
 
         # 最大待機時間（秒）
-        wait_time = 30
+        wait_time = 40
 
         # IDを入力
-        login_id_xpath = '//*[@id="identifierNext"]'
+        login_id_xpath = '//*[contains(@id, "identifierNext")]'
         # xpathの要素が見つかるまで待機します。
         WebDriverWait(driver, wait_time).until(
             EC.presence_of_element_located((By.XPATH, login_id_xpath)))
@@ -31,12 +30,11 @@ class AutoInput:
         driver.find_element_by_xpath(login_id_xpath).click()
 
         # パスワードを入力
-        login_pw_xpath = '//*[@id="passwordNext"]'
+        login_pw_xpath = '//*[contains(@id, "passwordNext")]'
         # xpathの要素が見つかるまで待機します。
         WebDriverWait(driver, wait_time).until(
             EC.presence_of_element_located((By.XPATH, login_pw_xpath)))
-        driver.find_element_by_name("password").send_keys(login_pw)
-        time.sleep(2)  # クリックされずに処理が終わるのを防ぐために追加。
+        driver.find_element_by_name("password").send_keys(login_pw)  # クリックされずに処理が終わるのを防ぐために追加。
         driver.find_element_by_xpath(login_pw_xpath).click()
 
     def input_googleform_1(driver):
@@ -49,7 +47,7 @@ class AutoInput:
         driver.find_element_by_xpath('//span[text()="次へ"]').click()
 
     def input_googleform_2(driver, temperature):
-        AVE_Temperature = 36.5
+        AVE_Temperature = 36.0
         i = int(temperature)
         f = int((temperature - i) * 10)
         driver.find_element_by_xpath("//div[@aria-label='" + str(i) + "']").click()
@@ -61,10 +59,9 @@ class AutoInput:
 
 
 if __name__ == '__main__':
-    AI = AutoInput
+    AI = GoogleFormInput
     # Chromeを起動
     driver = AI.start_chrome()
-
     # Googleにログイン
     AI.login_google(driver)
     time.sleep(4)
